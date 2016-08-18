810d8a00615da9be1ebcbbfe7e093a42cf5f9ee4
Nguyen Xuan Thanh <ThanhNguyen@Nguyens-MacBook-Pro.local>
2016-08-18 08:03:55 +0700

    Init project

---
+++ ShareMe/ShareMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShareMe
+//
+//  Created by Nguyen Xuan Thanh on 8/18/16.
+//  Copyright © 2016 Framgia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
